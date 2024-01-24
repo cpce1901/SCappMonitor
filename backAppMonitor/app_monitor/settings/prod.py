717f906470e6435cead477dafd5f2d41680e01c6
cpce1901 <ircd.claudio@gmail.com>
2024-01-24 12:48:18 -0300

    changePort

--- backAppMonitor/app_monitor/settings/prod.py
+++ backAppMonitor/app_monitor/settings/prod.py
@@ -18,6 +18,15 @@ DATABASES = {
     }
 }
 
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+    'DEFAULT_PARSER_CLASSES': (
+        'rest_framework.parsers.JSONParser',
+    )
+}
+
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
